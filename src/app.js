1769bada63ebbbc13bdcc6d1e484232326f5a6a5
Tobias Viana <eutobias@gmail.com>
2021-01-14 18:25:06 -0300

    mais logs

--- src/app.js
+++ src/app.js
@@ -222,7 +222,7 @@ function sendResponse(req, res, content, err = null) {
     console.log('sendResponse: ERROR')
     console.log(req.path)
     console.log(req.headers)
-    console.log(req.body)
+    console.log(req)
     console.log(err.response)
     console.log('>>>>>>>>>>')
 
